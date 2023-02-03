e910959056384ed8bd73cd11867397c22a277e90
kennethbchen <kennethbchen@gmail.com>
2023-02-03 17:21:50 -0500

    Implemented more complex animation

--- js/script.js
+++ js/script.js
@@ -20,7 +20,7 @@ function setWords() {
 
     // Pick random words
     let word1 = verbList.random();
-
+    
     // Get words that start with the same letter as word 1
     let filteredWords = verbList.filter((word) => word[0] === word1[0]);
 
@@ -28,19 +28,44 @@ function setWords() {
     let word2 = filteredWords.filter((word) => word !== word1).random();
     let word3 = filteredWords.filter((word) => word !== word1 && word !== word2).random();
 
-    word1Span.innerHTML = word1;
-    word2Span.innerHTML = word2;
-    word3Span.innerHTML = word3;
+    word1 += ", ";
+    word2 += ", ";
 
-    d3.selectAll(".word-container")
-        .interrupt()
-        .style("opacity", 0)
-        .style("bottom", "45px")
-        .transition()
-            .style("opacity", 1)
-            .style("bottom", "0px")
-            .ease(d3.easeBackOut)
-            .duration(500);
+    let wordData = [word1.split(""),word2.split(""),word3.split("")]
+
+    
+    // Set Text + Animation
+    let selection = d3.selectAll(".word-container").data(wordData);
+
+    // For each word container span
+    selection.each(function(word, i) {
+
+        // Give every letter in the word it's own span
+        let letterContainers = d3.select(this).selectAll(".letter-container").data(word).join("span")
+
+        // For each letter container span
+        letterContainers.each(function(letter, j) {
+            
+            // 
+            d3.select(this)
+            .attr("class", "letter-container")
+            .style("position", "relative")
+            .attr("position", j)
+            .text( d => d)
+            .interrupt()
+                .style("opacity", 0)
+                .style("bottom", "45px")
+                .transition()
+                    .style("opacity", 1)
+                    .style("bottom", "0px")
+                    .ease(d3.easeBackOut)
+                    .duration((i + 1) * 300 + j * 25);
+        });
+        
+
+        
+    });
+        
 
 }
 
